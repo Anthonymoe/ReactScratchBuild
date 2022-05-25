8dd82f9c67a2ec9d755c157c06a0eca367cbe616
Anthony Moe <anthony.moe7@gmail.com>
2022-05-24 20:17:41 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./src/App";
+
+ReactDOM = require('react-dom');
+
+ReactDOM.render(<App />, document.querySelector("#root"));
